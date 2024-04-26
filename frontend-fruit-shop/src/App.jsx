3339a2de7fc79f4b86950220961cad123ab3b1a7
Sebastian-Castaneda <juancastag1234@gmail.com>
2024-04-25 22:28:24 -0500

    Feature/API: Se consume la API REST anteriormente creada con fetch a localhost

--- frontend-fruit-shop/src/App.jsx
+++ frontend-fruit-shop/src/App.jsx
@@ -5,6 +5,7 @@ function App () {
   const [fruits, setFruits] = useState([])
 
   useEffect(() => {
+    
     fetch('http://localhost:3000/fruit')
       .then(res => res.json())
       .then(json =>
